73cc134c2f39e44b90d3811f1dae55b2ec9d8afc
rockstr99 <arpit150100@gmail.com>
2025-03-28 21:19:16 +0530

    initial commit

---
+++ src/main/java/com/rockstr99/config_manager_be/ConfigManagerBeApplication.java
@@ -0,0 +1,13 @@
+package com.rockstr99.config_manager_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigManagerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigManagerBeApplication.class, args);
+	}
+
+}
