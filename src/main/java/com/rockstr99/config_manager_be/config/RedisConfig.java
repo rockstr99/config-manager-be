d634f1d408487b510e5eede68beaee4aa4b3c40f
rockstr99 <arpit150100@gmail.com>
2025-03-28 22:58:45 +0530

    -added redis support -dependency added in pom -added redis configuration

---
+++ src/main/java/com/rockstr99/config_manager_be/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.rockstr99.config_manager_be.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate getRedisTemplate(RedisConnectionFactory redisConnectionFactory)
+    {
+        RedisTemplate redisTemplate= new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
